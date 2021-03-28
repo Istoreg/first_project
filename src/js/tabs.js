72e74094d11a92299b08f0f5ea602838013b3cd8
Igor Semenov <istoreg@gmail.com>
2021-03-28 23:25:36 +0700

    Create tabs.js

---
+++ src/js/tabs.js
@@ -0,0 +1,12 @@
+//табы для блока "Услуги и цены"
+(function($) {
+    $(function() {
+      
+      $('ul.price__list').on('click', 'li:not(.active)', function() {
+        $(this)
+          .addClass('active').siblings().removeClass('active')
+          .closest('div.price').find('div.price__content').removeClass('active').eq($(this).index()).addClass('active');
+      });
+      
+    });
+    })(jQuery);
