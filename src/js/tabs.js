01b7354a5c522326b2e27f722d69aa9bd975c681
Igor Semenov <istoreg@gmail.com>
2021-04-08 22:25:33 +0700

    Update tabs.js

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,12 +1,13 @@
+
 //табы для блока "Услуги и цены"
-(function($) {
-    $(function() {
-      
-      $('ul.price__list').on('click', 'li:not(.active)', function() {
-        $(this)
-          .addClass('active').siblings().removeClass('active')
-          .closest('div.price').find('div.price__content').removeClass('active').eq($(this).index()).addClass('active');
-      });
-      
+(function TabsBlock($) {
+  $(function() {
+    
+    $('ul.price__list').on('click', 'li:not(.active)', function() {
+      $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.price').find('div.price__content').removeClass('active').eq($(this).index()).addClass('active');
     });
-    })(jQuery);
+    
+  });
+  })(jQuery);
